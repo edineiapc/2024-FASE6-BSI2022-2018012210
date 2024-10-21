e8034a8a48c4d15493b7c46417acb0ef9eaeb882
edineiapc <48798059+edineiapc@users.noreply.github.com>
2024-10-21 01:15:19 +0000

    alteraçoes dos dois pontos para ponto e virgula

--- public/main.ts
+++ public/main.ts
@@ -1,23 +1,23 @@
 {
 
-  "main": "index.js",
-  "scripts": {
-    "start": "node dist/index.js",
-    "dev": "nodemon src/index.ts",
-    "build": "tsc -p ."
-  },
-  "keywords": [],
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "express": "^4.19.2",
-    "sqlite": "^5.1.1",
-    "sqlite3": "^5.1.7"
-  },
-  "devDependencies": {
-    "@types/express": "^4.17.21",
-    "nodemon": "^3.1.4",
-    "ts-node": "^10.9.2",
-    "typescript": "^5.5.4"
+  "main" ; "index.js"
+  "scripts"; {
+    "start"; "node dist/index.js"
+    "dev"; "nodemon src/index.ts"
+    "build"; "tsc -p ."
+  }
+  "keywords"; []
+  "author"; ""
+  "license"; "ISC"
+  "dependencies"; {
+    "express"; "^4.19.2"
+    "sqlite"; "^5.1.1"
+    "sqlite3"; "^5.1.7"
+  }
+  "devDependencies"; {
+    "@types/express"; "^4.17.21"
+    "nodemon"; "^3.1.4"
+    "ts-node"; "^10.9.2"
+    "typescript"; "^5.5.4"
   }
 }
